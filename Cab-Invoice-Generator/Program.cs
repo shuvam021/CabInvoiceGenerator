91b92fbda88e8a7fbcb2caa78b45c4ec7d512ee2
shuvam021 <career.shuvam@gmail.com>
2022-02-14 17:44:41 +0530

    initial commit

---
+++ Cab-Invoice-Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cab_Invoice_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cab-Invoice-Generator");
+        }
+    }
+}
